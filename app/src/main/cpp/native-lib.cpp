0d452c20650fc9b24fda48f9550bcf6f11bc5f3b
chenke <475148772@qq.com>
2022-12-27 23:31:48 +0800

    按钮提交

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,13 +3,17 @@
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_ffmodtest_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
-}extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_example_ffmodtest_ffmod_FFmodActivity_changeVoice(JNIEnv *env, jobject thiz,
-                                                           jstring voice_path, jint mode) {
-    // TODO: implement changeVoice()
+}
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_ffmodtest_ffmod_FFmodActivity_changeVoice(
+        JNIEnv *env, jobject thiz,
+        jstring voice_path, jint mode) {
+    //java字符串转c的字符串
+    const char *path = env->GetStringUTFChars(voice_path, 0);
+
 }
--- app/src/main/java/com/example/ffmodtest/ffmod/FFmodActivity.kt
+++ app/src/main/java/com/example/ffmodtest/ffmod/FFmodActivity.kt
@@ -1,41 +1,54 @@
 package com.example.ffmodtest.ffmod
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.blankj.utilcode.util.PathUtils
 import com.example.ffmodtest.databinding.ActivityFfmodBinding
+import org.fmod.FMOD
+import java.io.File
 
 class FFmodActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFfmodBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val path = PathUtils.getInternalAppFilesPath() + File.separator + "testffmod.mp3"
         binding = ActivityFfmodBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnPitchHalf.setOnClickListener {
+        binding.btnNormal.setOnClickListener {
+            changeVoice(path, 0)
+        }
 
+        binding.btnPitchHalf.setOnClickListener {
+            changeVoice(path, 1)
         }
         binding.btnPitchDouble.setOnClickListener {
-
+            changeVoice(path, 2)
         }
         binding.btnEcho.setOnClickListener {
-
+            changeVoice(path, 3)
         }
         binding.btnMixer.setOnClickListener {
-
+            changeVoice(path, 4)
         }
     }
 
-    external fun changeVoice(voicePath: String, mode: Int): String
+    override fun onResume() {
+        super.onResume()
+        FMOD.init(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        FMOD.close()
+    }
+
+    external fun changeVoice(voicePath: String, mode: Int)
 
     companion object {
         init {
             System.loadLibrary("native-lib")
         }
-
-        @JvmStatic
-        fun playComplete(playResult: String) {
-
-        }
     }
-
 }
